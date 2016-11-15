6e5d4e38afd12bfba301d372411f474839c9bf5c
William DeLude <qanwi1970@gmail.com>
2016-11-14 19:32:25 -0700

    Working with some hardcoded values

---
+++ DirectoryReplicator/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace DirectoryReplicator
+{
+    public static class Extensions
+    {
+        private static Action EmptyDelegate = delegate { };
+
+        public static void Refresh(this UIElement uiElement)
+        {
+            uiElement.Dispatcher.Invoke(DispatcherPriority.Background, EmptyDelegate);
+        }
+    }
+}
--- DirectoryReplicator/MainWindow.xaml.cs
+++ DirectoryReplicator/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DirectoryReplicator
 {
@@ -23,6 +13,51 @@ namespace DirectoryReplicator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            var destinationRoot = new DirectoryInfo(@"C:\Temp\dircopy");
+            var sourceRoot = new DirectoryInfo(@"C:\Users\qanwi\Documents");
+            foreach (ListBoxItem item in replicateList.Items)
+            {
+                var itemText = item.Content.ToString();
+                var folders = itemText.Split(new[] {" ==> "}, StringSplitOptions.None);
+                sourceRoot = new DirectoryInfo(folders[0]);
+                destinationRoot = new DirectoryInfo(folders[1]);
+                //CopyDirectoryContents(sourceRoot, destinationRoot);
+            }
+        }
+
+        private void CopyDirectoryContents(DirectoryInfo sourceRoot, DirectoryInfo destinationRoot)
+        {
+            foreach (var directoryInfo in sourceRoot.EnumerateDirectories())
+            {
+                lblProgress.Content = directoryInfo.Name;
+                lblProgress.Refresh();
+                var newDirectory = new DirectoryInfo(destinationRoot.FullName + "\\" + directoryInfo.Name);
+                if (!newDirectory.Exists)
+                {
+                    newDirectory.Create();
+                }
+                CopyDirectoryContents(directoryInfo, newDirectory);
+            }
+            foreach (var fileInfo in sourceRoot.EnumerateFiles())
+            {
+                lblProgress.Content = fileInfo.DirectoryName + "\\" + fileInfo.Name;
+                lblProgress.Refresh();
+                var newFile = destinationRoot.FullName + "\\" + fileInfo.Name;
+                if (File.Exists(newFile))
+                {
+                    // make sure it's not readonly
+                    var newFileInfo = new FileInfo(newFile);
+                    if (newFileInfo.IsReadOnly)
+                    {
+                        newFileInfo.IsReadOnly = false;
+                    }
+                }
+                fileInfo.CopyTo(newFile, true);
+            }
         }
     }
 }
