380f6235f7a2e4efffd15bb570d15a0c4bdc3513
William DeLude <qanwi1970@gmail.com>
2017-01-16 15:02:00 -0700

    fixing some small issues

--- src/python/Replicate.py
+++ src/python/Replicate.py
@@ -2,24 +2,51 @@ import json
 import os
 import sys
 from shutil import copy2
+import stat
+import logging
 
 
 def copy_directory_contents(src_dir, dst_dir):
-    print src_dir + " ==> " + dst_dir
     file_list = os.listdir(src_dir)
-    print "Files in directory: {0}".format(len(file_list))
     if not os.path.isdir(dst_dir):
         os.mkdir(dst_dir)
     for file_name in file_list:
         src_file = src_dir + "\\" + file_name
+        dst_file = dst_dir + "\\" + file_name
         if os.path.isfile(src_file):
-            print(file_name)
-            copy2(src_file, dst_dir)
+            if os.access(dst_file, os.F_OK):
+                dst_file_time = os.path.getmtime(dst_file)
+                src_file_time = os.path.getmtime(src_file)
+                if src_file_time > dst_file_time and not isclose(src_file_time, dst_file_time):
+                    print("File is newer: {0} > {1}".format(src_file_time, dst_file_time))
+                    if not os.access(dst_file, os.W_OK):
+                        os.chmod(dst_file, stat.S_IREAD | stat.S_IWRITE)
+                    copy_the_file(dst_dir, dst_file, src_file)
+            else:
+                print("File is new")
+                copy_the_file(dst_dir, dst_file, src_file)
         else:
-            copy_directory_contents(src_file, dst_dir + "\\" + file_name)
+            copy_directory_contents(src_file, dst_file)
+
+
+def copy_the_file(dst_dir, dst_file, src_file):
+    try:
+        print(dst_file)
+    except:
+        pass
+    try:
+        copy2(src_file, dst_dir)
+    except IOError:
+        logging.error("Error copying file {0} to {1}".format(src_file, dst_file))
+
+
+def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
+    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', filename='replicate.log', level=logging.DEBUG)
+    logging.info("Directory replication starting")
     print(sys.argv[1:])
     if len(sys.argv) > 1:
         settings_file_name = sys.argv[1]
@@ -27,7 +54,9 @@ if __name__ == '__main__':
         settings_file_name = "settings.json"
     with open(settings_file_name) as settings_file:
         settings = json.load(settings_file)
-    print(settings)
 
     for path in settings["paths"]:
+        logging.info("Copying directory contents from %s to %s", path["source"], path["destination"])
         copy_directory_contents(path["source"], path["destination"])
+
+    logging.info("Directory replication finished")
